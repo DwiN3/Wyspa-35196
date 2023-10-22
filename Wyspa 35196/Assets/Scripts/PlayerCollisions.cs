74945545e80e607100562d221abdd81c6893c05b
DwiN3 <dwinicht333@gmail.com>
2023-10-22 13:35:20 +0200

    dziala

--- Wyspa 35196/Assets/Scripts/PlayerCollisions.cs
+++ Wyspa 35196/Assets/Scripts/PlayerCollisions.cs
@@ -4,11 +4,6 @@ using UnityEngine;
 
 public class PlayerCollisions : MonoBehaviour
 {
-    bool doorIsOpen = false;
-    float doorTimer = 0.0f;
-    public float doorOpenTime = 3.0f;
-    public AudioClip doorOpenSound;
-    public AudioClip doorShutSound;
     GameObject currentDoor;
 
     // Start is called before the first frame update
@@ -20,29 +15,14 @@ public class PlayerCollisions : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (doorIsOpen)
+        RaycastHit hit;
+        if (Physics.Raycast(transform.position, transform.forward, out hit, 3))
         {
-            doorTimer += Time.deltaTime;
+            if (hit.collider.gameObject.tag == "playerDoor")
+            {
+                currentDoor = hit.collider.gameObject;
+                currentDoor.SendMessage("DoorCheck");
+            }
         }
-        if (doorTimer > doorOpenTime)
-        {
-            Door(doorShutSound, false, "doorshut", currentDoor);
-            doorTimer = 0.0f;
-        }
-    }
-    void OnControllerColliderHit(ControllerColliderHit hit)
-    {
-        if (hit.gameObject.tag == "playerDoor" && doorIsOpen == false)
-        {
-            currentDoor = hit.gameObject;
-            Door(doorOpenSound, true, "dooropen", currentDoor);
-        }
-    }
-
-    void Door(AudioClip aClip, bool openCheck, string animName, GameObject thisDoor)
-    {
-        doorIsOpen = openCheck;
-        thisDoor.GetComponent<AudioSource>().PlayOneShot(aClip);
-        thisDoor.transform.parent.GetComponent<Animation>().Play(animName);
     }
 }
