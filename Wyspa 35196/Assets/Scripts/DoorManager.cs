e266113b7dd54003eff9ad2a1124758cd72757f4
DwiN3 <dwinicht333@gmail.com>
2023-10-22 12:35:40 +0200

    dodanie plików do kosza

---
+++ Wyspa 35196/Assets/Scripts/DoorManager.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorManager : MonoBehaviour
+{
+    bool doorIsOpen = false;
+    float doorTimer = 0.0f;
+    public float doorOpenTime = 3.0f;
+    public AudioClip doorOpenSound;
+    public AudioClip doorShutSound;
+    GameObject currentDoor;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (doorIsOpen)
+        {
+            doorTimer += Time.deltaTime;
+        }
+        if (doorTimer > doorOpenTime)
+        {
+            Door(doorShutSound, false, "doorshut");
+            doorTimer = 0.0f;
+        }
+        RaycastHit hit;
+        if (Physics.Raycast(transform.position, transform.forward, out hit, 3))
+        {
+            if (hit.collider.gameObject.tag == "playerDoor")
+            {
+                currentDoor = hit.collider.gameObject;
+                currentDoor.SendMessage("DoorCheck");
+            }
+        }
+    }
+
+    void DoorCheck()
+    {
+        if (!doorIsOpen)
+        {
+            Door(doorOpenSound, true, "dooropen");
+        }
+    }
+
+    void Door(AudioClip aClip, bool openCheck, string animName)
+    {
+        doorIsOpen = openCheck;
+        currentDoor.GetComponent<AudioSource>().PlayOneShot(aClip);
+        currentDoor.transform.parent.GetComponent<Animation>().Play(animName);
+    }
+}
--- Wyspa 35196/Assets/Scripts/PlayerCollisions.cs
+++ Wyspa 35196/Assets/Scripts/PlayerCollisions.cs
@@ -4,11 +4,6 @@ using UnityEngine;
 
 public class PlayerCollisions : MonoBehaviour
 {
-    bool doorIsOpen = false;
-    float doorTimer = 0.0f;
-    public float doorOpenTime = 3.0f;
-    public AudioClip doorOpenSound;
-    public AudioClip doorShutSound;
     GameObject currentDoor;
 
     // Start is called before the first frame update
@@ -20,29 +15,7 @@ public class PlayerCollisions : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (doorIsOpen)
-        {
-            doorTimer += Time.deltaTime;
-        }
-        if (doorTimer > doorOpenTime)
-        {
-            Door(doorShutSound, false, "doorshut", currentDoor);
-            doorTimer = 0.0f;
-        }
+     
     }
-    void OnControllerColliderHit(ControllerColliderHit hit)
-    {
-        if (hit.gameObject.tag == "playerDoor" && doorIsOpen == false)
-        {
-            currentDoor = hit.gameObject;
-            Door(doorOpenSound, true, "dooropen", currentDoor);
-        }
-    }
-
-    void Door(AudioClip aClip, bool openCheck, string animName, GameObject thisDoor)
-    {
-        doorIsOpen = openCheck;
-        thisDoor.GetComponent<AudioSource>().PlayOneShot(aClip);
-        thisDoor.transform.parent.GetComponent<Animation>().Play(animName);
-    }
+    
 }
