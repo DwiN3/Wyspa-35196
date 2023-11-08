ae5d619c3457e0b191518660c3970e02d356bcf6
DwiN3 <dwinicht333@gmail.com>
2023-11-08 18:19:05 +0100

    dodanie zmieniania świateł

--- Wyspa 35196/Assets/Scripts/TriggerZone.cs
+++ Wyspa 35196/Assets/Scripts/TriggerZone.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class TriggerZone : MonoBehaviour
 {
     public AudioClip lockedSound;
+    public Light doorLight;
 
     // Start is called before the first frame update
     void Start()
@@ -15,7 +16,10 @@ public class TriggerZone : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (GameObject.Find("PowerGUI") && Inventory.charge == 4)
+        {
+            doorLight.color = Color.green;
+        }
     }
 
     void OnTriggerEnter(Collider col)
@@ -24,6 +28,7 @@ public class TriggerZone : MonoBehaviour
         {
             if (Inventory.charge == 4)
             {
+                Destroy(GameObject.Find("PowerGUI"));
                 transform.Find("door").SendMessage("DoorCheck");
             }
             else
