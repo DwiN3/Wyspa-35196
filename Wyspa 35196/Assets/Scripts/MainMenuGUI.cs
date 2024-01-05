5b19c70810553373f922b8cfabca6e71ff1b72ff
DwiN3 <dwinicht333@gmail.com>
2024-01-05 15:10:49 +0100

    dodanie skalowania

--- Wyspa 35196/Assets/Scripts/MainMenuGUI.cs
+++ Wyspa 35196/Assets/Scripts/MainMenuGUI.cs
@@ -18,12 +18,32 @@ public class MainMenuGUI : MonoBehaviour
     float buttonHeight = 40;
     float space = 20;
 
+    public bool adjustPosition;
+    public bool adjustSize;
+
+    float coefX = 1.0f;
+    float coefY = 1.0f;
+
     // Start is called before the first frame update
     void Start()
     {
-        playBtnRect = new Rect(50, 250, buttonWidth, buttonHeight);
-        instructionsBtnRect = new Rect(50, 250 + buttonHeight + space, buttonWidth, buttonHeight);
-        quitBtnRect = new Rect(50, 250 + (buttonHeight + space) * 2, buttonWidth, buttonHeight);
+        if (adjustPosition)
+        {
+            float w_2 = menuArea.width * 0.5f;
+            float h_2 = menuArea.height * 0.5f;
+            menuArea.x = (menuArea.x + w_2) * Screen.width / 1024 - w_2;
+            menuArea.y = (menuArea.y + h_2) * Screen.height / 768 - h_2;
+        }
+        if (adjustSize)
+        {
+            coefX = Screen.width / 1024.0f;
+            coefY = Screen.height / 768.0f;
+            menuArea.width *= coefX;
+            menuArea.height *= coefY;
+        }
+        playBtnRect = new Rect(50 * coefX, 250 * coefY, buttonWidth * coefX, buttonHeight * coefY);
+        instructionsBtnRect = new Rect(50 * coefX, (250 + buttonHeight + space) * coefY,buttonWidth * coefX, buttonHeight * coefY);
+        quitBtnRect = new Rect(50 * coefX, (250 + (buttonHeight + space) * 2) * coefY, buttonWidth * coefX, buttonHeight * coefY);
     }
 
     // Update is called once per frame
@@ -36,7 +56,7 @@ public class MainMenuGUI : MonoBehaviour
     {
         GUI.skin = menuSkin;
         GUI.BeginGroup(menuArea);
-        GUI.DrawTexture(new Rect(0, 0, 300, 211), gameLogo);
+        GUI.DrawTexture(new Rect(0, 0, 300 * coefX, 211 * coefY), gameLogo);
         if (GUI.Button(playBtnRect, "Play"))
         {
             Debug.Log("Naciœniêto start");
